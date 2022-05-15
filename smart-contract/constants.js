3dee6594e76921a64408157862e024b7dfaafd72
Raj Patil <rjpatil53@gmail.com>
2022-05-15 17:35:29 +0530

    Initial commit

---
+++ smart-contract/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+    "0x7269Ac89752445332ED3413F5d023297EaF8a744";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
